4efd61076b375b996e08a7cf2b2ad298f32b4a3e
HD <hd@Corinnes-MacBook-Pro.local>
2023-09-08 10:53:37 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,29 @@
+<!-- 
+Dans cet exercice, vous apprendrez à manipuler la syntaxe php.
+Veillez à bien lire les instructions présentes en commentaires.
+
+Good luck & happy coding !
+-->
+
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Made with ❤️ by HelloDev</title>
+    <!-- Ici, réaliser un lien avec le fichier styles.css -->
+
+    <!-- Ici, réaliser un lien avec le fichier main.js sans oublier l'attribut DEFER ou ASYNC -->
+
+</head>
+
+<body>
+    <h1 class="text-center py-10">Learn PHP - Step 0</h1>
+    <!-- Ici, réaliser une ouverture et une fermeture de balise de type PHP -->
+
+    <!-- Dans la balise PHP, déclarer une variable NAME ayant pour valeur votre prénom. Ensuite, effectuer un print grâce à la méthode ECHO -->
+</body>
+
+</html>
